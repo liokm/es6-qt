d267587b79aaa73c7bc63a2105e454ab7482a531
Meng <meng.li@eroad.com>
2017-06-01 11:47:12 +1200

    Keep it ES6

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+console.log('foobar')
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
